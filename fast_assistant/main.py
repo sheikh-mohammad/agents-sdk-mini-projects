7fb18d5f3a10602c1cef8459df6c84460cfa9436
Sheikh Mohammad Ahmed <sheikhmohammadahmed07@gmail.com>
2026-02-27 14:57:31 +0500

    Complete fast_assistant agent

--- fast_assistant/main.py
+++ fast_assistant/main.py
@@ -1,6 +1,29 @@
-def main():
-    print("Hello from fast-assistant!")
+from agents import Agent, Runner
+from dotenv import load_dotenv
+from os import getenv
+from openai import AsyncOpenAI
+from agents.models.openai_chatcompletions import OpenAIChatCompletionsModel
 
+load_dotenv()
 
-if __name__ == "__main__":
-    main()
+gemini_api = getenv("GEMINI_API_KEY")
+
+gemini_client: AsyncOpenAI = AsyncOpenAI(
+    api_key=gemini_api,
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/",
+)
+
+gemini_model: OpenAIChatCompletionsModel = OpenAIChatCompletionsModel(
+    model="gemini-2.5-flash", openai_client=gemini_client
+)
+
+fast_assistant: Agent = Agent(
+    name="Fast Assistant", instructions="You are a quick helper.", model=gemini_model
+)
+
+result = Runner.run_sync(
+    fast_assistant,
+    "What's 2 + 2?"
+)
+
+print(result.final_output)
