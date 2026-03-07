1c67c1841a5d29a48cb376a37aa3d9a70cf9a939
Sheikh Mohammad Ahmed <sheikhmohammadahmed07@gmail.com>
2026-03-07 13:26:48 +0500

    feat: implement Panaversity Support Agent with Chainlit

--- panaversity_support_agent/main.py
+++ panaversity_support_agent/main.py
@@ -1,6 +1,43 @@
-def main():
-    print("Hello from panaversity-support-agent!")
+from agents import Agent, Runner, RunConfig, OpenAIChatCompletionsModel
+from openai import AsyncOpenAI
+import chainlit as cl
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+gemini_api = os.getenv("GEMINI_API_KEY")
+
+gemini_client = AsyncOpenAI(
+    api_key=gemini_api,
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/",
+)
+
+gemini_model = OpenAIChatCompletionsModel(
+    model="gemini-2.5-flash", openai_client=gemini_client
+)
+
+config = RunConfig(
+    model=gemini_model, model_provider=gemini_client, tracing_disabled=True
+)
+
+agent = Agent(
+    name="Panaversity Support Agent",
+    instructions="You are a helpful assistant that can answer questions and tasks.",
+)
 
 
-if __name__ == "__main__":
-    main()
+@cl.on_chat_start
+async def first_message():
+
+    await cl.Message(
+        content="Hello! I'm Panaversity Support Agent. How can I help you today?"
+    ).send()
+
+
+@cl.on_message
+async def chat(message: cl.Message):
+
+    result = await Runner.run(agent, input=message.content, run_config=config)
+
+    await cl.Message(content=result.final_output).send()
